d52ddfbebbf1b10ddee4e112083e3e4c42d4db1d
GonzaloOchoa95 <gonzalo.afanador01@unicatolica.edu.co>
2024-10-26 13:42:16 -0500

    carga de front

---
+++ src/components/Form.jsx
@@ -0,0 +1,72 @@
+
+
+import './styles/Form.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Form({ callback, setiduser }) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [resultado, setResultado] = useState('');
+    const [texto11, setTexto11] = useState(''); // Necesitas inicializar este estado
+    
+    const navigate = useNavigate();
+
+    const registra = () => {
+        navigate('/registro');
+    };
+
+    const restablecerPassword = () => {
+        navigate('/restablecer');
+    };
+
+    const validateUser = async (event) => {
+        event.preventDefault();
+      
+        try {
+            const res = await fetch('http://localhost:4000/v1/signos/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+            });
+
+            const responseData = await res.json();
+            setResultado(responseData);
+
+            const { usuario, id } = responseData;
+
+            if (usuario === 'user') {
+                setiduser(id);
+                callback('user');
+                navigate('/userHome');
+            } else if (usuario === 'admin') {
+                callback('admin');
+                navigate('/adminHome');
+            } else if (responseData === 'usuario o contaseña invalida') {
+                setTexto11(responseData);
+            }
+        } catch (error) {
+            console.error('Error al validar el usuario:', error);
+            setTexto11('Error en la conexión');
+        }
+    };
+
+    return (
+        <form id="form1" onSubmit={validateUser}>
+            <h1 id="txtBienvenida">Bienvenido al portal de premios gana loco</h1>
+            <h4 className="txt">Nombre de Usuario</h4>
+            <input type="text" className="entry" onChange={(e) => setUsername(e.target.value)} /><br></br>
+            <h4 className="txt">Contraseña</h4>
+            <input type="password" className="entry" onChange={(e) => setPassword(e.target.value)} /><br></br>
+            <h6 id="texto">{texto11}</h6>
+            <input type="submit" value="Ingresar" id="btnEnviar" />
+            <button type="button" onClick={registra}>Registrate</button>
+            
+        </form>
+    );
+}
+
+export default Form;
+
---
+++ src/components/UserHome.jsx
@@ -0,0 +1,159 @@
+import { Navigate, useNavigate } from "react-router-dom";
+import './styles/UserHome.css';
+import TextSigno from "./TextSigno.jsx";
+import { useState, useEffect } from "react";
+
+function UserHome({user,iduser}){
+    if(user!=="user" || !user){ 
+        return <Navigate to="/"/>
+    }
+    const home = useNavigate();
+   
+    const [codigo, setcodigo] = useState({
+        numero: "",
+        usuario: iduser
+    });
+   
+    const [infoTabla, setInfoTabla] = useState([]);
+    const [texto, setTexto] = useState("");
+   
+
+    function goHome(){
+        home("/");
+    }
+
+    const AgregarCodigo = (event)=>{
+
+        const {name,value} = event.target;
+
+        setcodigo({...codigo,[name]:value})
+
+        setTexto(" ");
+
+      
+      
+        
+    }
+
+    useEffect(()=>{
+
+        fetch(`http://localhost:4000/v1/signos/traerusuario/${iduser}`, {
+            method: 'GET',
+            headers: {"Content-Type": "application/json"},
+            // body: JSON.stringify(null)
+        })
+        .then(response => response.json())
+                .then(responseData => {
+                    
+                    if (typeof responseData === "object"){
+                        
+                        setInfoTabla(responseData);
+                    }else{
+                        setTexto(responseData);
+                        
+                    }
+                })
+       
+
+     },[iduser,infoTabla])
+
+    
+    
+    async function handleSelect(event){
+        event.preventDefault();
+        
+        if(codigo.numero !== "0" && codigo.numero.trim() !== ""){
+            fetch(`http://localhost:4000/v1/signos/codigo`, {
+                method: 'POST',
+                headers: {"Content-Type":"application/json"},
+                body: JSON.stringify({codigo})
+            }) 
+                .then(response => response.json())
+                .then(responseData => {
+
+                    console.log(responseData)
+                    if (typeof responseData === "object"){
+                        
+                        setInfoTabla(prevInfoTabla=> [...prevInfoTabla,responseData]);
+                      
+                        console.log(infoTabla)
+                    }else{
+                        setTexto(responseData);
+                        
+                    }
+                })
+            } 
+            
+        }
+        useEffect(()=>{
+
+            console.log(infoTabla)
+           
+    
+         },[infoTabla])
+    
+        
+         return (
+            <div className="container">
+                <div id="txtSeleccionPage"><h3>REGISTRA TU CODIGO</h3></div>
+                <form action=""  onSubmit={handleSelect}>
+    
+                <label htmlFor="">Codigo</label>
+                <input 
+                    type="number"  
+                    name="numero" 
+                    maxLength="3" 
+                    min="0" 
+                    max="999" 
+                    onInput={(e) => {
+                        if(e.target.value.length > 3) {
+                        e.target.value = e.target.value.slice(0, 3);
+                        }
+                    }}  
+    
+                    value={codigo.numero}
+    
+                    onChange={AgregarCodigo}
+    
+                />
+    
+                <input type="submit" name = "agregar"/>
+    
+                </form>
+                <h3>{texto}</h3>
+                <button id="btnHome" onClick={goHome}>Inicio</button>
+    
+               {/* Mostrar la tabla solo si infoTabla tiene datos */}
+               {
+                infoTabla.length > 0 && (
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>FECHA</th>
+                                <th>CÓDIGO</th>
+                                <th>PREMIO</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* Mapeamos todos los registros en el array */}
+                            
+                            {infoTabla.length > 0 && (infoTabla.map((registro, index) => (
+                                <tr key={index}>
+                                    <td>{new Date(registro.fecha).toLocaleDateString()}</td>
+                                    <td>{registro.codigo}</td>
+                                    <td>{registro.premio}</td>
+                                </tr>
+                            )))}
+                        </tbody>
+                    </table>
+                )
+                }
+                  
+    
+                  
+            </div>
+        )
+       
+}
+
+export default UserHome;
